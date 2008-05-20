21bdbae674f943e3d66f3070808edab66e2f1e07
jochen <jochen@70ba6d20-ffbb-44b8-b480-e27427137dba>
2008-05-20 20:07:59 +0000

    Initial import.

---
+++ rifidi/tags/org.rifidi.emulator.reader.awid/1.0.0/src/org/rifidi/emulator/reader/awid/commandhandler/AwidAutonomous.java
@@ -0,0 +1,270 @@
+/*
+ *  AwidAutonomous.java
+ *
+ *  Created:	Jun 20, 2006
+ *  Project:	RiFidi Emulator - A Software Simulation Tool for RFID Devices
+ *  				http://www.rifidi.org
+ *  				http://rifidi.sourceforge.net
+ *  Copyright:	Pramari LLC and the Rifidi Project
+ *  License:	Lesser GNU Public License (LGPL)
+ *  				http://www.opensource.org/licenses/lgpl-license.html
+ */
+package org.rifidi.emulator.reader.awid.commandhandler;
+
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.rifidi.emulator.reader.awid.module.AwidReaderSharedResources;
+import org.rifidi.emulator.reader.awid.sharedrc.tagmemory.AwidTagMemory;
+import org.rifidi.emulator.reader.command.CommandObject;
+import org.rifidi.emulator.reader.module.abstract_.AbstractReaderSharedResources;
+import org.rifidi.emulator.reader.sharedrc.radio.generic.GenericRadio;
+import org.rifidi.emulator.tags.enums.TagGen;
+import org.rifidi.emulator.tags.impl.RifidiTag;
+import org.rifidi.utilities.formatting.ByteAndHexConvertingUtility;
+
+/**
+ * The autonomous commands. These commands are not called directly by the
+ * outside but instead called repeatedly within the simulation itself.
+ * 
+ * @author Matthew Dean - matt@pramari.com
+ * @since <$INITIAL_VERSION$>
+ * @version <$CURRENT_VERSION$>
+ */
+public class AwidAutonomous {
+
+	/**
+	 * The log4j logger for this class.
+	 */
+	private static Log logger = LogFactory.getLog(AwidAutonomous.class);
+
+	/**
+	 * Single tag meter for EPC C1 Gen2 tags.
+	 * 
+	 * @param arg
+	 *            The CommandObject which contains the information from the
+	 *            method.
+	 * @return The CommandObject, unmodified if the command was a get, modified
+	 *         if it was a set.
+	 */
+	public CommandObject single_epc_c1_gen2(CommandObject arg,
+			AbstractReaderSharedResources asr) {
+		logger.debug("calling single_epc_c1_gen2");
+
+		AwidReaderSharedResources awidSR = (AwidReaderSharedResources) asr;
+
+		GenericRadio newRadio = awidSR.getRadio();
+
+		logger.debug("before scan");
+
+		newRadio.scan(null, awidSR.getTagMemory());
+		logger.debug("after scan");
+
+		AwidTagMemory memory = (AwidTagMemory) asr.getTagMemory();
+		ArrayList<RifidiTag> tagList = (ArrayList<RifidiTag>) memory
+				.getTagReport();
+
+		ArrayList<Object> retVal = new ArrayList<Object>();
+
+		if (awidSR.isRf_power()) {
+			for (RifidiTag i : tagList) {
+				String returnValue = "15 20 00 ";
+				if (i.getTagType().equals(TagGen.GEN2)) {
+					returnValue += AwidCommon.formatTagString("30"
+							+ "00"
+							+ ByteAndHexConvertingUtility.toHexString(
+									i.getTag().readId()).replace(" ", "")
+							+ getCRC(i));
+					retVal.add(returnValue);
+				}
+			}
+		}
+		arg.setReturnValue(retVal);
+
+		return arg;
+	}
+
+	/**
+	 * Single tag meter for EPC C1 Gen1 tags.
+	 * 
+	 * @param arg
+	 *            The CommandObject which contains the information from the
+	 *            method.
+	 * @return The CommandObject, unmodified if the command was a get, modified
+	 *         if it was a set.
+	 */
+	public CommandObject single_epc_c1_gen1(CommandObject arg,
+			AbstractReaderSharedResources asr) {
+		logger.debug("Started gen1 meter");
+
+		AwidReaderSharedResources awidSR = (AwidReaderSharedResources) asr;
+
+		GenericRadio newRadio = awidSR.getRadio();
+
+		logger.debug("before scan");
+		newRadio.scan(null, awidSR.getTagMemory());
+
+		AwidTagMemory memory = (AwidTagMemory) asr.getTagMemory();
+		ArrayList<RifidiTag> tagList = (ArrayList<RifidiTag>) memory
+				.getTagReport();
+
+		ArrayList<Object> retVal = new ArrayList<Object>();
+
+		// TODO: Is this right? I don't think it is.
+		if (awidSR.isRf_power()) {
+			for (RifidiTag i : tagList) {
+				if (i.getTagType().equals(TagGen.GEN1)) {
+					retVal.add("12 16 00 "
+							+ ByteAndHexConvertingUtility.toHexString(i
+									.getTag().readId()));
+				}
+			}
+		}
+
+		for (Object i : retVal) {
+			logger.debug("RETURNING THIS TAG: " + i.toString());
+		}
+
+		arg.setReturnValue(retVal);
+
+		return arg;
+	}
+
+	/**
+	 * Tag meter for EPC C1 Gen1 tags.
+	 * 
+	 * @param arg
+	 *            The CommandObject which contains the information from the
+	 *            method.
+	 * @return The CommandObject, unmodified if the command was a get, modified
+	 *         if it was a set.
+	 */
+	public CommandObject epc_c1_gen1(CommandObject arg,
+			AbstractReaderSharedResources asr) {
+
+		AwidReaderSharedResources awidSR = (AwidReaderSharedResources) asr;
+
+		GenericRadio newRadio = awidSR.getRadio();
+
+		logger.debug("before scan");
+		newRadio.scan(null, awidSR.getTagMemory());
+		logger.debug("after scan");
+
+		AwidTagMemory memory = (AwidTagMemory) asr.getTagMemory();
+		ArrayList<RifidiTag> tagList = (ArrayList<RifidiTag>) memory
+				.getTagReport();
+
+		logger.debug("after the getTagList tagList size=" + tagList.size());
+
+		ArrayList<Object> retVal = new ArrayList<Object>();
+
+		if (awidSR.isRf_power()) {
+			for (RifidiTag i : tagList) {
+				if (i.getTagType().equals(TagGen.GEN1)) {
+					retVal.add("12 16 11 "
+							+ ByteAndHexConvertingUtility.toHexString(i
+									.getTag().readId()) + " 20");
+				}
+			}
+		}
+
+		logger.debug("after the adding of the strings retVal size = "
+				+ retVal.size());
+
+		arg.setReturnValue(retVal);
+
+		return arg;
+	}
+
+	/**
+	 * Tag meter for EPC C1 Gen2 tags.
+	 * 
+	 * @param arg
+	 *            The CommandObject which contains the information from the
+	 *            method.
+	 * @return The CommandObject, unmodified if the command was a get, modified
+	 *         if it was a set.
+	 */
+	public CommandObject epc_c1_gen2(CommandObject arg,
+			AbstractReaderSharedResources asr) {
+
+		AwidReaderSharedResources awidSR = (AwidReaderSharedResources) asr;
+
+		GenericRadio newRadio = awidSR.getRadio();
+
+		logger.debug("before scan");
+		newRadio.scan(null, awidSR.getTagMemory());
+		logger.debug("after scan");
+		AwidTagMemory memory = (AwidTagMemory) asr.getTagMemory();
+
+		ArrayList<RifidiTag> tagList = (ArrayList<RifidiTag>) memory
+				.getTagReport();
+
+		logger.debug("after the getTagList tagList size=" + tagList.size());
+
+		ArrayList<Object> retVal = new ArrayList<Object>();
+
+		if (awidSR.isRf_power()) {
+			for (RifidiTag i : tagList) {
+				if (i.getTagType().equals(TagGen.GEN2)) {
+					retVal.add("16 20 11 "
+							+ AwidCommon.formatTagString("30"
+									+ "00"
+									+ ByteAndHexConvertingUtility.toHexString(
+											i.getTag().readId()).replace(" ",
+											"") + getCRC(i)));
+				}
+			}
+		}
+
+		logger.debug("after the adding of the strings retVal size = "
+				+ retVal.size());
+
+		arg.setReturnValue(retVal);
+
+		return arg;
+	}
+
+	public String getCRC(RifidiTag tag) {
+		return calculateCRC(calculateProtocolArray("30", "00", tag.getTag()
+				.readId()));
+	}
+
+	private static byte[] calculateProtocolArray(String header,
+			String protocol, byte[] tagData) {
+
+		byte[] retVal = new byte[tagData.length + 2];
+
+		retVal[0] = Byte.parseByte(header, 16);
+		retVal[1] = Byte.parseByte(protocol, 16);
+		for (int i = 2; i < (tagData.length + 2); i++) {
+			retVal[i] = tagData[i - 2];
+		}
+		return retVal;
+	}
+
+	private static String calculateCRC(byte[] b) {
+		int crc;
+		crc = 0xFFFF;
+		for (byte bit : b) {
+			crc = ((int) bit << 8) ^ crc;
+			for (int i = 0; i < 8; i++) {
+				if ((crc & 0x8000) != 0) {
+					crc = (crc << 1) ^ 0x1021;
+				} else {
+					crc <<= 1;
+				}
+			}
+		}
+
+		int retInt = (crc ^ 0xFFFF);
+
+		String firstString = Integer.toHexString(retInt);
+
+		String lastString = firstString.substring(firstString.length() - 4,
+				firstString.length());
+
+		return lastString;
+	}
+}
---
+++ rifidi/tags/org.rifidi.emulator.reader.awid/1.0.0/src/org/rifidi/emulator/reader/awid/module/AwidReaderModule.java
@@ -0,0 +1,367 @@
+/*
+ *  AwidReaderModule.java
+ *
+ *  Project:		RiFidi Emulator - A Software Simulation Tool for RFID Devices
+ *  http://www.rifidi.org
+ *  http://rifidi.sourceforge.net
+ *  Copyright:	    Pramari LLC and the Rifidi Project
+ *  License:		Lesser GNU Public License (LGPL)
+ *  http://www.opensource.org/licenses/lgpl-license.html
+ */
+package org.rifidi.emulator.reader.awid.module;
+
+import gnu.io.SerialPort;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.rifidi.emulator.common.ControlSignal;
+import org.rifidi.emulator.common.PowerState;
+import org.rifidi.emulator.io.comm.CommunicationException;
+import org.rifidi.emulator.io.comm.manager.TagManager;
+import org.rifidi.emulator.io.comm.serial.SerialCommunication;
+import org.rifidi.emulator.io.comm.serial.SerialConnectionlessCommunicationConnectionState;
+import org.rifidi.emulator.io.comm.serial.SerialIOBean;
+import org.rifidi.emulator.io.comm.serial.SerialOffCommunicationPowerState;
+import org.rifidi.emulator.io.protocol.RawProtocol;
+import org.rifidi.emulator.reader.awid.command.exception.AwidExceptionHandler;
+import org.rifidi.emulator.reader.awid.formatter.AwidAutonomousCommandFormatter;
+import org.rifidi.emulator.reader.awid.formatter.AwidCommandFormatter;
+import org.rifidi.emulator.reader.awid.sharedrc.tagmemory.AwidTagMemory;
+import org.rifidi.emulator.reader.command.CommandObject;
+import org.rifidi.emulator.reader.command.controller.BasicCommandControllerOperatingState;
+import org.rifidi.emulator.reader.command.controller.autonomous.AutonomousCommandController;
+import org.rifidi.emulator.reader.command.controller.interactive.InteractiveCommandController;
+import org.rifidi.emulator.reader.command.controller.interactive.LoginAuthenticatedCommandControllerOperatingState;
+import org.rifidi.emulator.reader.command.xml.CommandXMLDigester;
+import org.rifidi.emulator.reader.control.adapter.CommandAdapter;
+import org.rifidi.emulator.reader.control.adapter.ReflectiveCommandAdapter;
+import org.rifidi.emulator.reader.control.adapter.searcher.RelativeCommandSearcher;
+import org.rifidi.emulator.reader.module.GeneralReaderPropertyHolder;
+import org.rifidi.emulator.reader.module.ReaderModule;
+import org.rifidi.emulator.reader.module.abstract_.AbstractPowerModule;
+import org.rifidi.emulator.reader.sharedrc.radio.Antenna;
+import org.rifidi.emulator.reader.sharedrc.radio.generic.GenericRadio;
+
+/**
+ * This class represents an AWID MPR reader.
+ * 
+ * @author Matthew Dean
+ * @since <$INITIAL_VERSION$>
+ * @version <$CURRENT_VERSION$>
+ */
+public class AwidReaderModule extends AbstractPowerModule implements
+		ReaderModule {
+
+	public static final String startupText = "<STARTUP>";
+
+	public static final String READERTYPE = "AwidMPR";
+
+	public static final String XMLLOCATION = "org/rifidi/emulator/reader/awid/module/";
+	
+	private boolean rf_power;
+
+	private TagManager tagManager;
+
+	/**
+	 * The log4j logger for this class.
+	 */
+	private static Log logger = LogFactory.getLog(AwidReaderModule.class);
+
+	/**
+	 * The baud to transfer at
+	 */
+	private static final int BAUD = 9600;
+
+	/**
+	 * The number of data bits to use
+	 */
+	private static final int DATA_BITS = SerialPort.DATABITS_8;
+
+	/**
+	 * The number of stop bits available
+	 */
+	private static final int STOP_BITS = SerialPort.STOPBITS_1;
+
+	/**
+	 * The number of parity bits available
+	 */
+	private static final int PARITY_BITS = SerialPort.PARITY_NONE;
+
+	/**
+	 * The flow control used
+	 */
+	private static final int FLOW_CONTROL = SerialPort.FLOWCONTROL_NONE;
+
+	/**
+	 * The max message length of the packet
+	 */
+	private static final int MAX_MESSAGE_LENGTH = 1024;
+
+	/**
+	 * The command adapter which the interactive mode uses when a user is in
+	 * authenticated mode.
+	 */
+	private CommandAdapter interactiveCommandAdapter;
+
+	/**
+	 * The command adapter for autonomous methods.
+	 */
+	private CommandAdapter autonomousCommandAdapter;
+
+	/**
+	 * The communication that this reader will use.
+	 */
+	private SerialCommunication serialCommunication;
+
+	/**
+	 * The interactive command controller for this. This allows the reader to
+	 * accept and process commands interactively.
+	 */
+	private InteractiveCommandController interactiveCommandController;
+
+	/**
+	 * The command controller for autonomous methods
+	 */
+	private AutonomousCommandController autonomousCommandController;
+
+	/**
+	 * The resources for this reader which are shared among controllers.
+	 */
+	private AwidReaderSharedResources sharedResources;
+
+	/**
+	 * The name of the reader
+	 */
+	private String name;
+
+	/**
+	 * The digester, gets and calls all commands.
+	 */
+	private CommandXMLDigester digester;
+
+	/**
+	 * Console logger for this class
+	 */
+	private Log consoleLogger = null;
+
+	/**
+	 * Blank constructor that should ONLY be used by sun services API!!!
+	 *
+	 */
+	public AwidReaderModule(){
+		
+	}
+	
+	/**
+	 * Constructor for AwidReaderModule. This represents an AWID MPR reader.
+	 * Properties: serial_port: a String that matches COM<number>
+	 * 
+	 * @param initialPowerState
+	 * @param powerControlSignal
+	 */
+	public AwidReaderModule(ControlSignal<Boolean> powerControlSignal,
+			GeneralReaderPropertyHolder properties) {
+		super(AwidReaderModuleOffPowerState.getInstance(), powerControlSignal);
+		rf_power=true;
+		this.name = properties.getReaderName();
+
+		// Get the digester ready
+		digester = new CommandXMLDigester();
+		digester.parseToCommand(this.getClass().getClassLoader()
+				.getResourceAsStream(XMLLOCATION + "reader.xml"));
+
+		consoleLogger = LogFactory.getLog("console."
+				+ properties.getReaderName());
+		consoleLogger.info(AwidReaderModule.startupText
+				+ "Instantiated AWID MPR with name: "
+				+ properties.getReaderName());
+		consoleLogger.info(AwidReaderModule.startupText
+				+ properties.getReaderName() + " Serial Port: "
+				+ properties.getProperty("serial_port"));
+		consoleLogger.info(AwidReaderModule.startupText
+				+ properties.getReaderName() + " has "
+				+ properties.getNumAntennas() + " antennas");
+
+		HashMap<Integer, Antenna> antennaHash = new HashMap<Integer, Antenna>();
+		String antennaSequence = "";
+		for (int i = 0; i < properties.getNumAntennas(); i++) {
+			// start antenna number at 1.
+			antennaHash.put(i, new Antenna(i, name));
+			antennaSequence += " " + i;
+		}
+		GenericRadio newRadio = new GenericRadio(antennaHash, 25, name);
+		// Map<String, Antenna> antennas = new HashMap<String, Antenna>();
+		// antennas.put("antenna1", new Antenna(255, null));
+		AwidTagMemory newTagMemory = new AwidTagMemory();
+
+
+		// Start the shared resources
+		AwidReaderSharedResources awidSharedResources = new AwidReaderSharedResources(
+				newRadio, newTagMemory, new ControlSignal<Boolean>(false),
+				new ControlSignal<Boolean>(false), new ControlSignal<Boolean>(
+						false), powerControlSignal, properties.getReaderName(),
+				digester.getAllCommands(), digester, new AwidExceptionHandler(), properties.getNumAntennas());
+
+		this.sharedResources = awidSharedResources;
+
+		try {
+			SerialIOBean newBean = new SerialIOBean((String) properties
+					.getProperty("serial_port"), BAUD, DATA_BITS, PARITY_BITS,
+					STOP_BITS, FLOW_CONTROL, MAX_MESSAGE_LENGTH);
+			this.serialCommunication = new SerialCommunication(
+					new RawProtocol(), this.sharedResources
+							.getInteractivePowerSignal(), this.sharedResources
+							.getInteractiveConnectionSignal(),
+					SerialOffCommunicationPowerState.getInstance(),
+					SerialConnectionlessCommunicationConnectionState
+							.getInstance(), newBean, this.name);
+
+		} catch (CommunicationException e) {
+			logger.error(e.getMessage());
+		}
+
+		interactiveCommandAdapter = new ReflectiveCommandAdapter(
+				"Interactive", new AwidCommandFormatter(), 
+				new AwidExceptionHandler(), this.sharedResources,
+				new RelativeCommandSearcher());
+
+		autonomousCommandAdapter = new ReflectiveCommandAdapter(
+				"Autonomous", new AwidAutonomousCommandFormatter(), 
+				new AwidExceptionHandler(), this.sharedResources,
+				new RelativeCommandSearcher());
+
+		interactiveCommandController = new InteractiveCommandController(
+				new LoginAuthenticatedCommandControllerOperatingState(
+						interactiveCommandAdapter), this.sharedResources
+						.getInteractivePowerSignal(), this.sharedResources
+						.getInteractiveConnectionSignal(),
+				this.serialCommunication);
+
+		Map<byte[], Integer> autoCommands = new HashMap<byte[], Integer>();
+		autoCommands.put(null, 300);
+
+		autonomousCommandController = new AutonomousCommandController(
+				new BasicCommandControllerOperatingState(
+						autonomousCommandAdapter), this.sharedResources
+						.getAutonomousPowerSignal(), null,
+				this.serialCommunication, autoCommands);
+
+		this.sharedResources.saveAllSharedProperties();
+		// start the xmlrpc-server for this reader
+		// logger.info("Starting XmlRpc-server on: "
+		// + properties.getXmlRpcAddress());
+		// tagManager = new TagManager(properties.getXmlRpcAddress());
+		// logger.info("Started XmlRpc-server on: "
+		// + properties.getXmlRpcAddress());
+	}
+
+	/**
+	 * Exposes the underlying method to package members.
+	 * 
+	 * @see org.rifidi.emulator.reader.module.abstract_.AbstractPowerModule#changePowerState(org.rifidi.emulator.common.PowerState)
+	 */
+	@Override
+	protected void changePowerState(PowerState anotherPowerState) {
+		logger.debug("Changing power state...");
+		super.changePowerState(anotherPowerState);
+	}
+
+	/**
+	 * @return the interactiveCommandAdapter
+	 */
+	public CommandAdapter getInteractiveCommandAdapter() {
+		return interactiveCommandAdapter;
+	}
+
+	/**
+	 * Exposes the underlying method to package members.
+	 * 
+	 * @see org.rifidi.emulator.reader.module.abstract_.AbstractPowerModule#getPowerControlSignal()
+	 */
+	@Override
+	protected ControlSignal<Boolean> getPowerControlSignal() {
+		return super.getPowerControlSignal();
+	}
+
+	/**
+	 * Returns the sharedResources for the AWID reader.
+	 * 
+	 * @return the sharedResources
+	 */
+	public AwidReaderSharedResources getSharedResources() {
+		return sharedResources;
+	}
+
+	/**
+	 * @return the serialCommunication
+	 */
+	public SerialCommunication getInteractiveCommunication() {
+		return serialCommunication;
+	}
+
+	/**
+	 * @return the serialCommunication
+	 */
+	public InteractiveCommandController getInteractiveCommandController() {
+		return interactiveCommandController;
+	}
+
+	/**
+	 * Returns the Autonomous Command Controller.
+	 * 
+	 * @return the Autonomous Command Controller.
+	 */
+	public AutonomousCommandController getAutonomousCommandController() {
+		return autonomousCommandController;
+	}
+
+	/**
+	 * Returns the Autonomous Command Adapter.
+	 * 
+	 * @return the Autonomous Command Adapter.
+	 */
+	public CommandAdapter getAutonomousCommandAdapter() {
+		return autonomousCommandAdapter;
+	}
+
+	/**
+	 * Returns the name of the reader.
+	 * 
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	public HashMap<String, CommandObject> getCommandList() {
+		return digester.getAllCommands();
+	}
+
+	public Collection<String> getAllCategories() {
+		return digester.getAllCategories();
+	}
+
+	public Collection<CommandObject> getCommandsByCategory(String category) {
+		return digester.getCommandsByCategory(category);
+	}
+
+	/**
+	 * Finalize the reader
+	 */
+	public void finalize() {
+		tagManager.stop();
+	}
+
+	public boolean isRf_power() {
+		return rf_power;
+	}
+
+	public void setRf_power(boolean rf_power) {
+		this.rf_power = rf_power;
+	}
+
+}
