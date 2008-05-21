2fe248cdfdaa7815c36a271c784ff86e29418888
kyle <kyle@70ba6d20-ffbb-44b8-b480-e27427137dba>
2008-05-21 08:26:54 +0000

    Fixed tag registry.  Filled in some method stubs

--- rifidi/trunk/org.rifidi.emulator.reader.symbol/src/org/rifidi/emulator/reader/symbol/tagbuffer/SymbolTagMemory.java
+++ rifidi/trunk/org.rifidi.emulator.reader.symbol/src/org/rifidi/emulator/reader/symbol/tagbuffer/SymbolTagMemory.java
@@ -16,9 +16,9 @@ import java.util.Collection;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.rifidi.emulator.reader.sharedrc.tagmemory.RifidiTagMap;
 import org.rifidi.emulator.reader.sharedrc.tagmemory.TagMemory;
 import org.rifidi.services.tags.impl.RifidiTag;
+import org.rifidi.services.tags.utils.RifidiTagMap;
 
 /**
  * This is a SymbolTagMemory class that provides a tag memory for symbol. It is taken
