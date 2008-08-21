5e681475c5234ea747587f2d43d0326fd0f13eb4
jochen <jochen@70ba6d20-ffbb-44b8-b480-e27427137dba>
2008-08-21 10:31:03 +0000

    cleanup

--- rifidi/trunk/org.rifidi.designer.rcp/src/org/rifidi/designer/services/core/highlighting/HighlightingService.java
+++ rifidi/trunk/org.rifidi.designer.rcp/src/org/rifidi/designer/services/core/highlighting/HighlightingService.java
@@ -10,7 +10,6 @@
  */
 package org.rifidi.designer.services.core.highlighting;
 
-import java.util.List;
 import java.util.Set;
 
 import org.rifidi.designer.entities.VisualEntity;
@@ -18,7 +17,7 @@ import org.rifidi.designer.entities.VisualEntity;
 import com.jme.renderer.ColorRGBA;
 
 /**
- * 
+ * Service for applying renderstates for highlighting.
  * 
  * @author Jochen Mader - jochen@pramari.com - Jun 30, 2008
  * 
@@ -27,25 +26,29 @@ public interface HighlightingService {
 
 	/**
 	 * Change the highlight state of entities.
+	 * 
 	 * @param color
 	 * @param highlight
 	 */
 	void changeHighlighting(ColorRGBA color, Set<VisualEntity> highlight);
-	
+
 	/**
-	 * Change the highlight of already highlighted entities. 
+	 * Change the highlight of already highlighted entities.
+	 * 
 	 * @param color
 	 * @param newcolor
 	 * @param entity
 	 */
-	void changeHighlightColor(ColorRGBA color, ColorRGBA newcolor, Set<VisualEntity> hilight);
-	
+	void changeHighlightColor(ColorRGBA color, ColorRGBA newcolor,
+			Set<VisualEntity> hilight);
+
 	/**
 	 * Remove highlighting from the given set of entities.
+	 * 
 	 * @param hilight
 	 */
 	void clearHighlights(Set<VisualEntity> hilight);
-	
+
 	/**
 	 * Remove all highlights.
 	 */
