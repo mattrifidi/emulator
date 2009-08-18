95340ae0618fca20e3db92d053550ec63419c5b4
matt <matt@70ba6d20-ffbb-44b8-b480-e27427137dba>
2009-08-18 18:08:00 +0000

    Tag before removing serial.

---
+++ rifidi/tags/org.rifidi.emulator.reader.awid/2.2.3/src/org/rifidi/emulator/reader/awid/sharedrc/radio/AwidTimer.java
@@ -0,0 +1,27 @@
+/*
+ *  AwidTimer.java
+ *
+ *  Project:		RiFidi Emulator - A Software Simulation Tool for RFID Devices
+ *  http://www.rifidi.org
+ *  http://rifidi.sourceforge.net
+ *  Copyright:	    Pramari LLC and the Rifidi Project
+ *  License:		Lesser GNU Public License (LGPL)
+ *  http://www.opensource.org/licenses/lgpl-license.html
+ */
+package org.rifidi.emulator.reader.awid.sharedrc.radio;
+
+/**
+ * Timer for the Awid MPR reader.  
+ *
+ * @author Matthew Dean
+ * @since  <$INITIAL_VERSION$>
+ * @version <$CURRENT_VERSION$>
+ */
+public class AwidTimer {
+	/**
+	 * Constructor for the Awid reader (blank)
+	 */
+	public AwidTimer() {
+		
+	}
+}
